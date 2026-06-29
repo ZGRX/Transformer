88aba38e3964446ff2ff7a80689b55e7276b2d12
ricey <1796962310@qq.com>
2026-06-30 02:45:06 +0800

    Add scaled dot-product attention function

---
+++ attention_utils.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn.functional as F
+from math import sqrt
+
+def scaled_dot_product_attention(query, key, value, query_mask=None, key_mask=None, mask=None):
+    dim_k = query.size(-1)
+    scores = torch.bmm(query, key.transpose(1, 2)) / sqrt(dim_k)
+    if query_mask is not None and key_mask is not None:
+        mask = torch.bmm(query_mask.unsqueeze(-1), key_mask.unsqueeze(1))
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, -float("inf"))
+    weights = F.softmax(scores, dim=-1)
+    return torch.bmm(weights, value)
